1884c614f1af781dbfee63a96b709bce90036f2e
yalong-tse <yalong.tse@gmail.com>
2013-11-24 23:38:51 +0800

    add production detail function

--- src/com/rolling/money/ProductDetailActivity.java
+++ src/com/rolling/money/ProductDetailActivity.java
@@ -1,29 +1,21 @@
 package com.rolling.money;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.widget.TextView;
 
-/**
- * 我的资产
- *
- */
-public class PropertyActivity extends Activity {
+public class ProductDetailActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_property);
-		
-		TextView textView = (TextView)findViewById(R.id.title);
-		textView.setText("我的资产");
+		setContentView(R.layout.activity_product_detail);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.property, menu);
+		getMenuInflater().inflate(R.menu.product_detail, menu);
 		return true;
 	}
 
