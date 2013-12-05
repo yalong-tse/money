8561bf8c889648bf395f4c800d77d1c3e1677417
yalong-tse <yalong.tse@gmail.com>
2013-12-06 00:24:23 +0800

    modify main activity

--- src/com/rolling/money/MainActivity.java
+++ src/com/rolling/money/MainActivity.java
@@ -20,7 +20,12 @@ public class MainActivity extends Activity {
 	// Gridview 中的菜单
 	private List<HashMap<String,Object>> menuitems = new ArrayList<HashMap<String,Object>>();
 
-	private String[] items = {"我的资产","金融超市","资产配置","理财圈","个人设置","网点查询","每日推荐","风险测试","理财计算器","理财资讯","客服咨询","女士专区","我的收藏"};
+	private String[] items = {
+			"金融超市","理财资讯","每日推荐","理财圈子",
+			"我的资产","风险评估","资产配置","网点查询",
+			"循环投资","境外投资","女士专区","理财计算器",
+			"个人设置","我的收藏","客服咨询"
+			};
 	
 	private GridView gridview;
 	
@@ -33,32 +38,38 @@ public class MainActivity extends Activity {
 		for(int i=0;i<items.length;i++)
 		{
 			HashMap<String,Object> map = new HashMap<String,Object>();
-			if(i==0)
+			if(items[i].equalsIgnoreCase("我的资产"))
 				map.put("image", R.drawable.module21);
-			else if(i==1)
+			else if(items[i].equalsIgnoreCase("金融超市"))
 				map.put("image", R.drawable.module13);
-			else if(i==2)
+			else if(items[i].equalsIgnoreCase("资产配置"))
 				map.put("image", R.drawable.module20);
-			else if(i==3)
+			else if(items[i].equalsIgnoreCase("理财圈子"))
 				map.put("image", R.drawable.module6);
-			else if (i==4)
+			else if (items[i].equalsIgnoreCase("每日推荐"))
 				map.put("image", R.drawable.module1);
-			else if(i==5)
+			else if(items[i].equalsIgnoreCase("网点查询"))
 				map.put("image", R.drawable.module8);
-			else if(i==6)
+			else if(items[i].equalsIgnoreCase("每日推荐"))
 				map.put("image", R.drawable.module5);
-			else if(i==7)
+			else if(items[i].equalsIgnoreCase("风险评估"))
 				map.put("image", R.drawable.module3);
-			else if(i==8)
+			else if(items[i].equalsIgnoreCase("理财计算器"))
 				map.put("image", R.drawable.module23);
-			else if(i==9)
+			else if(items[i].equalsIgnoreCase("理财资讯"))
 				map.put("image", R.drawable.module9);
-			else if(i==10)
+			else if(items[i].equalsIgnoreCase("客服咨询"))
 				map.put("image", R.drawable.module15);
-			else if(i==11)
+			else if(items[i].equalsIgnoreCase("智能筛选"))
 				map.put("image", R.drawable.module10);
-			else if(i==12)
+			else if(items[i].equalsIgnoreCase("我的收藏"))
 				map.put("image", R.drawable.module14);
+			else if(items[i].equalsIgnoreCase("循环投资"))
+				map.put("image", R.drawable.module24);
+			else if(items[i].equalsIgnoreCase("境外投资"))
+				map.put("image", R.drawable.module10);
+			else
+				map.put("image", R.drawable.module16);
 			map.put("item", items[i]);
 			
 			menuitems.add(map);
@@ -86,81 +97,81 @@ public class MainActivity extends Activity {
 				Log.d("money", "the index is :" + arg2);
 				switch (arg2)
 				{
-				// 我的资产
-				case 0:
-					Intent intent_property = new Intent();
-					intent_property.setClass(MainActivity.this, PropertyActivity.class);
-					startActivity(intent_property);
-					break;
 				// 金融超市
-				case 1:
+				case 0:
 					Intent intent_product = new Intent();
 					intent_product.setClass(MainActivity.this, ListProductActivity.class);
 					startActivity(intent_product);
 					break;
-				// 资产配置
+				// 理财咨询
+				case 1:
+					Intent intent_news = new Intent();
+					intent_news.setClass(MainActivity.this, NewsActivity.class);
+					startActivity(intent_news);
+					break;
+				// 每日推荐
 				case 2:
-					Intent intent_property_config = new Intent();
-					intent_property_config.setClass(MainActivity.this, PropertyConfigActivity.class);
-					startActivity(intent_property_config);
+					Intent intent_recommend = new Intent();
+					intent_recommend.setClass(MainActivity.this,RecommendActivity.class);
+					startActivity(intent_recommend);
 					break;
 					
-				// 理财圈
+				// 理财圈子
 				case 3:
 					Intent intent_circle = new Intent();
 					intent_circle.setClass(MainActivity.this, CircleActivity.class);
 					startActivity(intent_circle);
 					break;
 					
-				// 个人设置
+				// 我的资产
 				case 4:
-					Intent intent_person = new Intent();
-					intent_person.setClass(MainActivity.this, PersonInfoActivity.class);
-					startActivity(intent_person);
+					Intent intent_property = new Intent();
+					intent_property.setClass(MainActivity.this, PropertyActivity.class);
+					startActivity(intent_property);
 					break;
 					
-				// 网点查询
+				// 风险评估 
 				case 5:
+					Intent intent_risktesting = new Intent();
+					intent_risktesting.setClass(MainActivity.this, RiskTestingActivity.class);
+					startActivity(intent_risktesting);
+					break;
+					
+				// 资产配置
+				case 6:
+					Intent intent_property_config = new Intent();
+					intent_property_config.setClass(MainActivity.this, PropertyConfigActivity.class);
+					startActivity(intent_property_config);
+					break;
+				// 网点查询
+				case 7:
 					Intent intent_map = new Intent();
 					intent_map.setClass(MainActivity.this, MapviewActivity.class);
 					startActivity(intent_map);
 					
 					break;
-					
-				// 每日推荐
-				case 6:
-					Intent intent_recommend = new Intent();
-					intent_recommend.setClass(MainActivity.this,RecommendActivity.class);
-					startActivity(intent_recommend);
-					
-					break;
-				//风险测试
-				case 7:
-					Intent intent_risktesting = new Intent();
-					intent_risktesting.setClass(MainActivity.this, RiskTestingActivity.class);
-					startActivity(intent_risktesting);
-					break;
 				// 理财计算器
 				case 8:
-					Intent intent_cal = new Intent();
-					intent_cal.setClass(MainActivity.this, CalActivity.class);
-					startActivity(intent_cal);
+					
 					break;
-				// 理财资讯
+				// 
 				case 9:
-					Intent intent_news = new Intent();
-					intent_news.setClass(MainActivity.this, NewsActivity.class);
-					startActivity(intent_news);
 					
 					break;
 				// 客服咨询
 				case 10:
 					break;
-				// 女士专区
+				// 理财计算器
 				case 11:
+					Intent intent_cal = new Intent();
+					intent_cal.setClass(MainActivity.this, CalActivity.class);
+					startActivity(intent_cal);
 					break;
-				// 我的收藏
+				// 个人设置
 				case 12:
+					Intent intent_person = new Intent();
+					intent_person.setClass(MainActivity.this, PersonInfoActivity.class);
+					startActivity(intent_person);
 					break;
 				// 
 				case 13:
