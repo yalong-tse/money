ff9e500888458c48430c662e0405d3f77e82508f
nhr407 <nhr407@gmail.com>
2013-12-05 23:15:20 +0800

    add contact me

--- src/com/rolling/money/PropertyConfigActivity.java
+++ src/com/rolling/money/PropertyConfigActivity.java
@@ -1,11 +1,14 @@
 package com.rolling.money;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-public class PropertyConfigActivity extends Activity {
+public class PropertyConfigActivity extends FragmentActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,23 @@ public class PropertyConfigActivity extends Activity {
 		
 		TextView textView = (TextView)findViewById(R.id.title);
 		textView.setText("×Ê²úÅäÖÃ");
+		
+		TextView contactme = (TextView)findViewById(R.id.config_consultant_contactme);
+		
+		contactme.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showMyDialog();
+			}
+
+			private void showMyDialog() {
+				FragmentManager fm = getSupportFragmentManager();
+			  	LicaiDialogFragment dialog = LicaiDialogFragment.newInstance();
+			  	
+			  	dialog.show(fm, "fragment_edit_name");
+			}
+		});
+		
 	}
 
 	@Override
