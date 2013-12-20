4a1b21e4de237766c8f5aec18b1737884b63318c
nhr407 <nhr407@gmail.com>
2013-12-20 17:03:25 +0800

    modify layout

--- src/com/rolling/money/LoginActivity.java
+++ src/com/rolling/money/LoginActivity.java
@@ -15,7 +15,7 @@ public class LoginActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
+//		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
 
--- src/com/rolling/money/SplashScreenActivity.java
+++ src/com/rolling/money/SplashScreenActivity.java
@@ -105,7 +105,7 @@ public class SplashScreenActivity extends Activity implements OnViewChangeListen
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.splash_screen, menu);
+//		getMenuInflater().inflate(R.menu.splash_screen, menu);
 		return true;
 	}
 
