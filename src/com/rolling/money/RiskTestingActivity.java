eb89d66c32df2d589e716b01dd951d23e94e4387
nhr407 <nhr407@gmail.com>
2013-12-27 09:27:34 +0800

    modify risktesting activity

--- src/com/rolling/money/RiskTestingActivity.java
+++ src/com/rolling/money/RiskTestingActivity.java
@@ -1,8 +1,12 @@
 package com.rolling.money;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.rolling.money.data.MyPagerAdapter;
@@ -27,7 +31,20 @@ public class RiskTestingActivity extends BaseBarActivity {
 		TabPageIndicator tabindicator = (TabPageIndicator) findViewById(R.id.titles);
 		tabindicator.setViewPager(pager);
 	
+		// 客户风向评价
+		LinearLayout risktest = (LinearLayout) findViewById(R.id.btn_risktest);
+		risktest.setOnClickListener(startrisktest);
+		
 	}
+	
+	private OnClickListener startrisktest = new OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			Intent intent = new Intent(RiskTestingActivity.this, PropertyConfigActivity.class);
+			startActivity(intent);
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
